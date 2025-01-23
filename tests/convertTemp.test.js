3325e9a6764495a92ddfdc656330e346310971a8
kazuki-0418 <kazuki.castle0418@gmail.com>
2025-01-23 11:15:12 -0800

    Exercise is done

--- tests/convertTemp.test.js
+++ tests/convertTemp.test.js
@@ -3,6 +3,16 @@
 // Test case 2: Converting 104 deg fahrenheit will return 40 deg celsius
 // Both test cases need to pass
 
-const Converter = require('../src/convertTemp')
+const Converter = require("../src/convertTemp");
 
-const convert = new Converter()
+const convert = new Converter();
+
+describe("Temperature conversion", () => {
+  test(`should return 86 when converting 30 deg celsius to fahrenheit`, () => {
+    expect(convert.convertToFahrenheit(30)).toBe(86);
+  });
+
+  test(`should return 20 when converting 68 deg fahrenheit to celsius`, () => {
+    expect(convert.convertToCelsius(68)).toBe(20);
+  });
+});
--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,7 +1,23 @@
 // Create two test cases for incrementing and decrementing a number
-// Test case 1: counter(10, 'increment') will eventualy return 10
+// Test case 1: counter(10, 'increment') will eventually return 10
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
-const counter = require('../src/counter')
+const counter = require("../src/counter");
+
+// testing the code that use setTimeout
+describe("Counter", () => {
+  test("should return 4 when incrementing 4", () => {
+    return expect(counter(4, "increment")).resolves.toBe(4);
+  });
+
+  test("should return 0 when decrementing 4", () => {
+    return expect(counter(4, "decrement")).resolves.toBe(0);
+  });
+
+  test("should return not -2 when decrementing 0 ", async () => {
+    const result = counter(0, "decrement");
+    await expect(result).resolves.not.toBe(-2);
+  });
+});
